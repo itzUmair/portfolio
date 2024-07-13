f4b7e9094b62d39b578f73f671b1b5c458d00b87
umair <muhammadumair2511@gmail.com>
2024-07-13 12:39:16 +0500

    fix(app): fixed hero section responsiveness

--- components/Hero.tsx
+++ components/Hero.tsx
@@ -3,7 +3,7 @@ import Image from "next/image";
 
 export default function Hero() {
   return (
-    <section className="mt-16">
+    <section className="h-screen pt-32 md:h-auto md::mt-16">
       <h2 className="text-xl lg:text-2xl">Hey visitor!</h2>
       <h1 className="font-bold text-4xl md:text-6xl lg:text-8xl leading-[60px] md:leading-[80px] lg:leading-[130px]">Welcome To <br /> The Portfolio Of <br /> Muhammad Umair Khan</h1>
       <div className="flex gap-x-8 my-4">
@@ -11,7 +11,7 @@ export default function Hero() {
         <Button content="Let&apos;s Connect" image="/arrow.png"/>
       </div>
       <div className="flex justify-center">
-        <button className="my-8"><Image src="/scroll-icon.png" alt="scroll" width={50} height={50} className="w-9 h-9 lg:w-12 lg:h-12" /></button>
+        <button className="my-8"><Image src="/scroll-icon.png" alt="scroll" width={50} height={50} className="w- lg:w-12 lg:h-12" /></button>
       </div>
     </section>
   )
