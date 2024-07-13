1bb8ba015ba4670b4b5b4f8bb079799097b44880
umair <muhammadumair2511@gmail.com>
2024-07-13 17:57:54 +0500

    fix(app): refactored header

--- components/Header.tsx
+++ components/Header.tsx
@@ -1,20 +1,16 @@
-import Image from "next/image"
+import Image from "next/image";
+import Link from "next/link";
 
 export default function Header() {
   return (
-    <nav className="flex justify-between items-center my-4 lg:my-8">
-        <Image
-          src="/logo.png"
-          alt="Umair"
-          width={50}
-          height={50}
-          className="w-10 h-10 lg:w-12 lg:h-12"
-        />
-        <ul className="flex gap-x-4 lg:text-2xl">
-          <li>About</li>
-          <li>Projects</li>
-          <li>Contact</li>
-        </ul>
+    <nav className="my-4 lg:my-8">
+      <Image
+        src="/logo.png"
+        alt="Umair"
+        width={50}
+        height={50}
+        className="w-10 h-10 lg:w-12 lg:h-12"
+      />
     </nav>
-  )
+  );
 }
